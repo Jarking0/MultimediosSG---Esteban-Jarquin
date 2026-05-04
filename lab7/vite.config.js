4ff6060a282dbbd7cb6fd684dc971cf376a5e08a
Esteban Jarquin <esteban.jarquin01@gmail.com>
2026-05-04 15:33:52 -0600

    Ultima parte Laboratorio 7

---
+++ lab7/vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: "src", 
+  base: "/MultimediosSG---Esteban-Jarquin/lab7", 
+  build: {
+    outDir: "../dist", 
+  }
+});
